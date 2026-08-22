83a0fc3960fdfb488445fdeb139537ab638db123
rokeyaag <lutforitsolution@gmail.com>
2026-08-22 23:27:19 +0600

    fix: resolve Platform reference and remove font monkeypatch in App.js

--- App.js
+++ App.js
@@ -1,46 +1,19 @@
 import "react-native-gesture-handler";
 import React, { useEffect, useState, useRef } from "react";
-import { AppState } from "react-native";
-import { Text, TextInput } from "react-native";
+import { AppState, Text, TextInput, ActivityIndicator, View, Platform } from "react-native";
 import { useFonts, NotoSansBengali_400Regular, NotoSansBengali_700Bold } from "@expo-google-fonts/noto-sans-bengali";
-import { ActivityIndicator, View } from "react-native";
 import { AuthProvider } from "./src/context/AuthContext";
 import { LanguageProvider } from "./src/context/LanguageContext";
 import { registerForPushNotifications } from "./src/services/notifications";
 import { getPin } from "./src/services/security";
 import AppNavigator from "./src/navigation/AppNavigator";
 import PinLockScreen from "./src/screens/PinLockScreen";
-const applyGlobalFont = () => {
-  if (Platform.OS === "web") return;
-  const oldTextRender = Text?.render;
-  if (oldTextRender) {
-    Text.render = function(...args) {
-      const origin = oldTextRender.call(this, ...args);
-      return React.cloneElement(origin, {
-        style: [{ fontFamily: "NotoSansBengali_400Regular" }, origin?.props?.style],
-      });
-    };
-  }
-  const oldTextInputRender = TextInput?.render;
-  if (oldTextInputRender) {
-    TextInput.render = function(...args) {
-      const origin = oldTextInputRender.call(this, ...args);
-      return React.cloneElement(origin, {
-        style: [{ fontFamily: "NotoSansBengali_400Regular" }, origin?.props?.style],
-      });
-    };
-  }
-};
-let fontApplied = false;
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     NotoSansBengali_400Regular,
     NotoSansBengali_700Bold,
   });
-  if (fontsLoaded && !fontApplied) {
-    applyGlobalFont();
-    fontApplied = true;
-  }
   const [locked, setLocked] = useState(false);
   const [pinChecked, setPinChecked] = useState(false);
   const appState = useRef(AppState.currentState);
